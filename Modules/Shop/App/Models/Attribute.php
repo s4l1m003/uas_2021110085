5c9aad367ce14e1897b2907c54235e4e7b45f196
jonathan salim <jonathansalim003@gmail.com>
2024-11-25 22:39:45 +0700

    initial

---
+++ Modules/Shop/App/Models/Attribute.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Shop\App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Shop\Database\factories\AttributeFactory;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [];
+    
+    protected static function newFactory(): AttributeFactory
+    {
+        //return AttributeFactory::new();
+    }
+}
